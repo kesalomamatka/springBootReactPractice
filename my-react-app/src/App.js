ee4b7155964dbe389bd64a2e21f5d8b2f742be38
Zhihao <paradisetan798@gmail.com>
2019-09-18 22:44:45 +0300

    router

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import NavBar from "./components/NavBar";
 import ProjectBoard from "./components/ProjectBoard";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import AddProjectTask from "./components/ProjectTask/AddProjectTask";
+
 class App extends Component{
   render() {
     return(
-        <div className="App">
-            <NavBar></NavBar>
-            <ProjectBoard></ProjectBoard>
-        </div>
+        <Router>
+            <div className="App">
+                <NavBar />
+                <Route exact path="/" component={ProjectBoard}/>
+
+                <Route exact path="/addProjectTask" component={AddProjectTask}/>
+            </div>
+        </Router>
     );
   }
 
