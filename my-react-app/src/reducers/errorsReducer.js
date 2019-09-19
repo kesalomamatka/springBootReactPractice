a026f09a2e143234f39c1674fada4021bf150992
Zhihao <paradisetan798@gmail.com>
2019-09-19 21:40:58 +0300

    reducer

---
+++ my-react-app/src/reducers/errorsReducer.js
@@ -0,0 +1,15 @@
+import {GET_ERROR} from "../actions/types";
+
+const initialState = {error: "test error"}
+
+export default function(state = initialState, action){
+    switch (action.type) {
+        case GET_ERROR:
+            return action.payload;
+
+        default:
+            return state;
+
+
+    }
+}
